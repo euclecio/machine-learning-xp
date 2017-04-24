2f1de79402a4c9546c63fe1fcaf1694fc9ae6fa3
Euclécio Josias Rodrigues <eucjosias@gmail.com>
2017-04-23 23:05:23 -0300

    More datasets and algoritham improvements

---
+++ k-nearest-neighbors/calcfunctions.py
@@ -0,0 +1,44 @@
+def euclideanDistance(instance1, instance2, length):
+    distance = 0
+    for x in range(length):
+        distance += pow((instance1[x] - instance2[x]), 2)
+    return math.sqrt(distance)
+
+def getNeighbors(trainingSet, testInstance, k):
+    distances = []
+    length = len(testInstance)-1
+    for x in range(len(trainingSet)):
+        dist = euclideanDistance(testInstance, trainingSet[x], length)
+        distances.append((trainingSet[x], dist))
+    # Sort by lower distance
+    distances.sort(key=operator.itemgetter(1))
+    # Append the k neighbors with lower distance
+    neighbors = []
+    for x in range(k):
+        neighbors.append(distances[x][0])
+    return neighbors
+
+def getResponse(neighbors):
+    classVotes = {}
+    for x in range(len(neighbors)):
+        response = neighbors[x][-1]
+        if response in classVotes:
+            classVotes[response] += 1
+        else:
+            classVotes[response] = 1
+    # Sort by most voted neighbors
+    sortedVotes = sorted(classVotes.items(), key=operator.itemgetter(1), reverse=True)
+    return sortedVotes[0][0]
+
+def getAccuracy(testSet, predictions):
+    correct = 0
+    wrong = 0
+    for x in range(len(testSet)):
+        if testSet[x][-1] == predictions[x]:
+            correct += 1
+        else:
+            wrong += 1
+
+    print('Correct: ' + repr(correct))
+    print('Wrong: ' + repr(wrong))
+    return (correct/float(len(testSet))) * 100.0
--- k-nearest-neighbors/knn.py
+++ k-nearest-neighbors/knn.py
@@ -5,83 +5,69 @@ import csv
 import random
 import math
 import operator
+from pprint import pprint
+from calcfunctions import euclideanDistance, getNeighbors, getResponse, getAccuracy
 
-def loadDataset(filename, split, trainingSet=[] , testSet=[]):
+def loadDataset(filename):
+    classSet = {}
     with open(filename, 'r') as csvfile:
         lines = csv.reader(csvfile)
         dataset = list(lines)
         for x in range(len(dataset)):
-            for y in range(4):
+            # Turn attrs into float except the class name
+            nAttrs = len(dataset[x])
+            for y in range(nAttrs - 1):
                 dataset[x][y] = float(dataset[x][y])
-            # Randomly select which set to add the instance
-            if random.random() < split:
-                trainingSet.append(dataset[x])
+            # Split by class
+            className = dataset[x][nAttrs - 1]
+            className.replace('\'', '')
+            if className not in classSet:
+                classSet[className] = []
+            classSet[className].append(dataset[x])
+    return classSet
+
+def splitSets(filename, trainingZ1Set=[], trainingZ2Set=[], testZ3Set=[]):
+    classSet = loadDataset(filename)
+    trainRate = 0.50
+    for key, value in classSet.items():
+        for x in range(len(value)):
+            # Randomly select an instance to add to the set
+            if random.random() < trainRate:
+                trainingZ1Set.append(value[x])
             else:
-                testSet.append(dataset[x])
+                testZ3Set.append(value[x])
 
-
-def euclideanDistance(instance1, instance2, length):
-    distance = 0
-    for x in range(length):
-        distance += pow((instance1[x] - instance2[x]), 2)
-    return math.sqrt(distance)
-
-def getNeighbors(trainingSet, testInstance, k):
-    distances = []
-    length = len(testInstance)-1
-    for x in range(len(trainingSet)):
-        dist = euclideanDistance(testInstance, trainingSet[x], length)
-        distances.append((trainingSet[x], dist))
-    # Sort by lower distance
-    distances.sort(key=operator.itemgetter(1))
-    # Append the k neighbors with lower distance
-    neighbors = []
-    for x in range(k):
-        neighbors.append(distances[x][0])
-    return neighbors
-
-def getResponse(neighbors):
-    classVotes = {}
-    for x in range(len(neighbors)):
-        response = neighbors[x][-1]
-        if response in classVotes:
-            classVotes[response] += 1
-        else:
-            classVotes[response] = 1
-    # Sort by desc most voted neighbors
-    sortedVotes = sorted(classVotes.items(), key=operator.itemgetter(1), reverse=True)
-    return sortedVotes[0][0]
-
-def getAccuracy(testSet, predictions):
-    correct = 0
-    wrong = 0
-    for x in range(len(testSet)):
-        if testSet[x][-1] == predictions[x]:
-            correct += 1
-        else:
-            wrong += 1
-
-    print('Correct: ' + repr(correct))
-    print('Wrong: ' + repr(wrong))
-    return (correct/float(len(testSet))) * 100.0
+    print('Train set: ' + repr(len(trainingZ1Set)))
+    print('Test set: ' + repr(len(testZ3Set)))
 
 def main():
     # Prepare data
-    trainingSet=[]
-    testSet=[]
-    split = 0.50
-    loadDataset('../data/iris.data', split, trainingSet, testSet)
-    print('Train set: ' + repr(len(trainingSet)))
-    print('Test set: ' + repr(len(testSet)))
-    # Generate predictions
-    predictions=[]
-    k = 3
-    for x in range(len(testSet)):
-        neighbors = getNeighbors(trainingSet, testSet[x], k)
-        result = getResponse(neighbors)
-        predictions.append(result)
-        print('Predicted=' + repr(result) + ' > Actual=' + repr(testSet[x][-1]))
-    accuracy = getAccuracy(testSet, predictions)
-    print('Accuracy: ' + repr(accuracy) + '%')
+    z1TrainingSet = []
+    z2TrainingSet = []
+    z3TestSet = []
+    splitSets('../data/iris.data', z1TrainingSet, z2TrainingSet, z3TestSet)
+
+    # print('Train set: ' + repr(len(trainingSet)))
+    # print('Test set: ' + repr(len(testSet)))
+
+    # # Generate predictions
+    # predictions=[]
+    # k = 3
+    # table = "| Predicted | Actual |\n";
+    # table = table + "| --------- | ------ |\n"
+    # for x in range(len(testSet)):
+    #     neighbors = getNeighbors(trainingSet, testSet[x], k)
+    #     result = getResponse(neighbors)
+    #     predictions.append(result)
+
+    #     if result != testSet[x][-1]:
+    #         row = "| **%s** | **%s** |\n" % (repr(result), repr(testSet[x][-1]))
+    #     else:
+    #         row = "| %s | %s |\n" % (repr(result), repr(testSet[x][-1]))
+
+    #     table = table + row
+    # # print(table)
+    # accuracy = getAccuracy(testSet, predictions)
+    # print('Accuracy: ' + repr(accuracy) + '%')
 
 main()
